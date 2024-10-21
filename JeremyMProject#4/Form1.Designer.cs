4ec6bd5d04a63698b6ccddf4f54e56e774ccf310
Exertible <Student@A414-10>
2024-10-21 11:56:17 -0400

    ICA 5

--- JeremyMProject#4/Form1.Designer.cs
+++ JeremyMProject#4/Form1.Designer.cs
@@ -31,115 +31,214 @@
             lblGame = new Label();
             txtQuantity = new TextBox();
             lblQuantity = new Label();
-            TotalCost = new ListBox();
+            lstOutput = new ListBox();
             btnCalc = new Button();
             btnClear = new Button();
             btnQuit = new Button();
             lblPrice = new Label();
             txtPrice = new TextBox();
+            lblGameName = new Label();
+            txtGameName = new TextBox();
+            groupBox1 = new GroupBox();
+            radioButton1 = new RadioButton();
+            radioButton2 = new RadioButton();
+            radioButton3 = new RadioButton();
+            groupBox1.SuspendLayout();
             SuspendLayout();
             // 
             // lblGame
             // 
             lblGame.AutoSize = true;
-            lblGame.Font = new Font("Castellar", 15.75F, FontStyle.Bold | FontStyle.Underline, GraphicsUnit.Point, 0);
-            lblGame.ForeColor = Color.Red;
-            lblGame.Location = new Point(333, 25);
+            lblGame.Font = new Font("MS Reference Sans Serif", 18F, FontStyle.Bold | FontStyle.Underline, GraphicsUnit.Point, 0);
+            lblGame.ForeColor = Color.Green;
+            lblGame.Location = new Point(234, 9);
             lblGame.Name = "lblGame";
-            lblGame.Size = new Size(84, 25);
-            lblGame.TabIndex = 1;
-            lblGame.Text = "Game";
+            lblGame.Size = new Size(314, 29);
+            lblGame.TabIndex = 0;
+            lblGame.Text = "Video Game Distributor";
+            lblGame.TextAlign = ContentAlignment.TopCenter;
             lblGame.Click += txtGame_Click;
             // 
             // txtQuantity
             // 
-            txtQuantity.Location = new Point(336, 96);
+            txtQuantity.Location = new Point(357, 222);
             txtQuantity.Name = "txtQuantity";
             txtQuantity.Size = new Size(81, 23);
-            txtQuantity.TabIndex = 2;
+            txtQuantity.TabIndex = 8;
+            txtQuantity.TextChanged += txtQuantity_TextChanged;
             // 
             // lblQuantity
             // 
             lblQuantity.AutoSize = true;
-            lblQuantity.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            lblQuantity.Location = new Point(221, 98);
+            lblQuantity.Font = new Font("MS Reference Sans Serif", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblQuantity.ForeColor = SystemColors.ControlText;
+            lblQuantity.Location = new Point(218, 221);
             lblQuantity.Name = "lblQuantity";
-            lblQuantity.Size = new Size(106, 21);
-            lblQuantity.TabIndex = 3;
+            lblQuantity.Size = new Size(133, 20);
+            lblQuantity.TabIndex = 7;
             lblQuantity.Text = "Quantity (#):";
             // 
-            // TotalCost
+            // lstOutput
             // 
-            TotalCost.FormattingEnabled = true;
-            TotalCost.ItemHeight = 15;
-            TotalCost.Location = new Point(165, 155);
-            TotalCost.Name = "TotalCost";
-            TotalCost.Size = new Size(462, 139);
-            TotalCost.TabIndex = 4;
+            lstOutput.Font = new Font("MS Reference Sans Serif", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lstOutput.FormattingEnabled = true;
+            lstOutput.Location = new Point(85, 253);
+            lstOutput.Name = "lstOutput";
+            lstOutput.Size = new Size(622, 148);
+            lstOutput.TabIndex = 9;
+            lstOutput.TabStop = false;
+            lstOutput.SelectedIndexChanged += lstOutput_SelectedIndexChanged;
             // 
             // btnCalc
             // 
-            btnCalc.Location = new Point(165, 300);
+            btnCalc.Font = new Font("MS Reference Sans Serif", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnCalc.ForeColor = Color.Black;
+            btnCalc.Location = new Point(85, 407);
             btnCalc.Name = "btnCalc";
-            btnCalc.Size = new Size(98, 40);
-            btnCalc.TabIndex = 5;
-            btnCalc.Text = "&Total Cost Calculate";
+            btnCalc.Size = new Size(118, 31);
+            btnCalc.TabIndex = 10;
+            btnCalc.Text = "&Total Cost";
             btnCalc.UseVisualStyleBackColor = true;
-            btnCalc.Click += button1_Click;
+            btnCalc.Click += btnCalc_Click;
             // 
             // btnClear
             // 
-            btnClear.Location = new Point(345, 300);
+            btnClear.Font = new Font("MS Reference Sans Serif", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnClear.ForeColor = Color.Black;
+            btnClear.Location = new Point(334, 407);
             btnClear.Name = "btnClear";
-            btnClear.Size = new Size(103, 27);
-            btnClear.TabIndex = 6;
+            btnClear.Size = new Size(118, 31);
+            btnClear.TabIndex = 11;
             btnClear.Text = "&Clear";
             btnClear.UseVisualStyleBackColor = true;
             btnClear.Click += btnClear_Click;
             // 
             // btnQuit
             // 
-            btnQuit.Location = new Point(534, 300);
+            btnQuit.Font = new Font("MS Reference Sans Serif", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnQuit.ForeColor = Color.Black;
+            btnQuit.Location = new Point(589, 407);
             btnQuit.Name = "btnQuit";
-            btnQuit.Size = new Size(93, 27);
-            btnQuit.TabIndex = 7;
+            btnQuit.Size = new Size(118, 31);
+            btnQuit.TabIndex = 12;
             btnQuit.Text = "&Quit";
             btnQuit.UseVisualStyleBackColor = true;
+            btnQuit.Click += btnQuit_Click;
             // 
             // lblPrice
             // 
             lblPrice.AutoSize = true;
-            lblPrice.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            lblPrice.Location = new Point(250, 65);
+            lblPrice.BackColor = Color.Transparent;
+            lblPrice.Font = new Font("MS Reference Sans Serif", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblPrice.ForeColor = SystemColors.ControlText;
+            lblPrice.Location = new Point(256, 179);
             lblPrice.Name = "lblPrice";
-            lblPrice.Size = new Size(77, 21);
-            lblPrice.TabIndex = 8;
+            lblPrice.Size = new Size(95, 20);
+            lblPrice.TabIndex = 5;
             lblPrice.Text = "Price ($):";
             // 
             // txtPrice
             // 
-            txtPrice.Location = new Point(336, 67);
+            txtPrice.Location = new Point(357, 180);
             txtPrice.Name = "txtPrice";
             txtPrice.Size = new Size(81, 23);
-            txtPrice.TabIndex = 9;
+            txtPrice.TabIndex = 6;
+            txtPrice.TextChanged += txtPrice_TextChanged;
+            txtPrice.Enter += txtPrice_Enter;
+            txtPrice.Leave += txtPrice_Leave;
+            // 
+            // lblGameName
+            // 
+            lblGameName.AutoSize = true;
+            lblGameName.Font = new Font("MS Reference Sans Serif", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblGameName.ForeColor = SystemColors.ControlText;
+            lblGameName.Location = new Point(225, 140);
+            lblGameName.Name = "lblGameName";
+            lblGameName.Size = new Size(126, 20);
+            lblGameName.TabIndex = 3;
+            lblGameName.Text = "Game Name:";
+            // 
+            // txtGameName
+            // 
+            txtGameName.Location = new Point(357, 140);
+            txtGameName.Name = "txtGameName";
+            txtGameName.Size = new Size(81, 23);
+            txtGameName.TabIndex = 4;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(radioButton1);
+            groupBox1.Controls.Add(radioButton2);
+            groupBox1.Controls.Add(radioButton3);
+            groupBox1.Location = new Point(85, 53);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(622, 71);
+            groupBox1.TabIndex = 11;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Game Genres";
+            groupBox1.Enter += groupBox1_Enter;
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Location = new Point(20, 36);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(157, 19);
+            radioButton1.TabIndex = 0;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "Role Playing Game (RPG)";
+            radioButton1.UseVisualStyleBackColor = true;
+            radioButton1.CheckedChanged += RPG_CheckedChanged;
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.Location = new Point(249, 36);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(160, 19);
+            radioButton2.TabIndex = 1;
+            radioButton2.TabStop = true;
+            radioButton2.Text = "First Person Shooter (FPS)";
+            radioButton2.UseVisualStyleBackColor = true;
+            radioButton2.CheckedChanged += radioButton2_CheckedChanged;
+            // 
+            // radioButton3
+            // 
+            radioButton3.AutoSize = true;
+            radioButton3.Location = new Point(504, 36);
+            radioButton3.Name = "radioButton3";
+            radioButton3.Size = new Size(61, 19);
+            radioButton3.TabIndex = 2;
+            radioButton3.TabStop = true;
+            radioButton3.Text = "Racing";
+            radioButton3.UseVisualStyleBackColor = true;
+            radioButton3.CheckedChanged += radioButton3_CheckedChanged;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.Control;
             ClientSize = new Size(800, 450);
+            Controls.Add(groupBox1);
+            Controls.Add(txtGameName);
+            Controls.Add(lblGameName);
             Controls.Add(txtPrice);
             Controls.Add(lblPrice);
             Controls.Add(btnQuit);
             Controls.Add(btnClear);
             Controls.Add(btnCalc);
-            Controls.Add(TotalCost);
+            Controls.Add(lstOutput);
             Controls.Add(lblQuantity);
             Controls.Add(txtQuantity);
             Controls.Add(lblGame);
+            ForeColor = SystemColors.ControlText;
             Name = "Form1";
-            Text = "Game";
+            Text = "Distributor";
             TopMost = true;
+            Load += Form1_Load;
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -148,11 +247,17 @@
         private Label lblGame;
         private TextBox txtQuantity;
         private Label lblQuantity;
-        private ListBox TotalCost;
+        private ListBox lstOutput;
         private Button btnCalc;
         private Button btnClear;
         private Button btnQuit;
         private Label lblPrice;
         private TextBox txtPrice;
+        private Label lblGameName;
+        private TextBox txtGameName;
+        private GroupBox groupBox1;
+        private RadioButton radioButton1;
+        private RadioButton radioButton2;
+        private RadioButton radioButton3;
     }
 }
--- JeremyMProject#4/Form1.cs
+++ JeremyMProject#4/Form1.cs
@@ -1,14 +1,72 @@
+//Jeremy Mohan Kalawan ICA 5
+using System.CodeDom;
+
 namespace JeremyMProject_4
 {
     public partial class Form1 : Form
     {
+        private string GameGenre;
+        const string RPG = "Role Playing Game (RPG)";
+        const string FPS = "First Person Shooter (FPS)";
+        const string Racing = "Racing";
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void btnCalc_Click(object sender, EventArgs e)
         {
+            btnCalc.Focus();
+
+            // Variables
+            double GamePrice, totalGamePrice;
+            int GameQuantity;
+            bool PriceValid, QuantityValid;
+            string GameName;
+            double GameGenreDiscount = 0;
+
+            // Input & Parsing
+            // GamePrice = double.Parse(txtPrice.Text);
+            // GameQuantity = int.Parse(txtQuantity.Text);
+            GameName = txtGameName.Text;
+            PriceValid = double.TryParse(txtPrice.Text, out GamePrice);
+            QuantityValid = int.TryParse(txtQuantity.Text, out GameQuantity);
+            if (PriceValid && QuantityValid)
+            {
+                // Switch
+                switch (GameGenre)
+                {
+                    case RPG:
+                        GameGenreDiscount = 0.25;
+                        break;
+
+                    case FPS:
+                        GameGenreDiscount = 0.50;
+                        break;
+                    
+                    case Racing:
+                        GameGenreDiscount = 0.75;
+                        break;
+
+                    default:
+                        lstOutput.Items.Add("This should never happen.");
+                        break;
+                }
+                // Processing
+                totalGamePrice = (GamePrice * GameGenreDiscount) * GameQuantity;
+
+                // Output
+                lstOutput.Items.Add("Your discount is " + GameGenreDiscount.ToString("P0"));
+                lstOutput.Items.Add("The total price for " + GameName + " is " + totalGamePrice.ToString("C2"));
+            }
+            
+            else
+            {
+                if (!PriceValid)
+                    lstOutput.Items.Add("Please enter a numeric number for the price.");
+                if (!QuantityValid)
+                    lstOutput.Items.Add("Please enter a whole number for the quantity.");
+            }
 
         }
 
@@ -19,7 +77,95 @@ namespace JeremyMProject_4
 
         private void btnClear_Click(object sender, EventArgs e)
         {
+            txtGameName.Clear();
+            txtPrice.Clear();
+            txtQuantity.Clear();
+            lstOutput.Items.Clear();
+            txtGameName.Focus();
+            txtPrice.Focus();
+            txtQuantity.Focus();
+            lstOutput.Items.Clear();
+            radioButton1.Checked = true;
+        }
 
+        private void btnQuit_Click(object sender, EventArgs e)
+        {
+            // Brings up a pop up box when you click quit.
+            DialogResult ButtonSelected;
+            ButtonSelected = MessageBox.Show(
+                "Do you really want to quit this program?", "Exiting Distributor...",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Warning);
+            if (ButtonSelected == DialogResult.Yes)
+            {
+                // Closes the program.
+                this.Close();
+            }
+
+        }
+
+        private void lstOutput_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtPrice_Enter(object sender, EventArgs e)
+        {
+            txtPrice.BackColor = Color.White;
+        }
+
+        private void txtPrice_Leave(object sender, EventArgs e)
+        {
+            txtPrice.BackColor = Color.White;
+        }
+
+        private void txtPrice_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtGameName_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtQuantity_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            radioButton1.Checked = true;
+        }
+
+        private void RPG_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+            {
+                GameGenre = RPG;
+            }
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton2.Checked)
+            {
+                GameGenre = FPS;
+            }
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton3.Checked)
+            {
+                GameGenre = Racing;
+            }
         }
     }
 }
